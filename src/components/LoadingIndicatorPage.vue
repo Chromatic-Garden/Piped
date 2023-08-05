22b59087723aa063ed24b4edfd645c533110ffac
Lily Rose <lilyrose2798@gmail.com>
2023-08-05 12:25:34 +1000

    Change loading indicator width to match new video player width

--- src/components/LoadingIndicatorPage.vue
+++ src/components/LoadingIndicatorPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="!showContent" class="min-h-[75vh] w-full flex items-center justify-center">
+    <div v-if="!showContent" class="min-h-[80vh] w-full flex items-center justify-center">
         <span id="spinner" />
     </div>
     <div v-else>
