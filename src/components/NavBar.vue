620719813819c29645837d7f49e81d21c0703cbf
Lily Rose <lilyrose2798@gmail.com>
2023-08-05 12:23:01 +1000

    Change nav bar padding

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="relative w-full flex flex-wrap items-center justify-center px-2 pb-2.5 sm:px-4">
+    <nav class="relative w-full flex flex-wrap items-center justify-center px-2.5 py-2 sm:px-4">
         <div class="flex flex-1 justify-start">
             <router-link class="flex items-center text-3xl font-bold font-sans" :to="homePagePath"
                 ><img
